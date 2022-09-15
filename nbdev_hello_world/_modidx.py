f804d4de4f60478615059c70da33ed4892c5eb49
Carlos E. GALINDEZ <cgalindez@gmail.com>
2022-09-15 14:05:45 -0300

    Add `say_hello`; update index

--- nbdev_hello_world/_modidx.py
+++ nbdev_hello_world/_modidx.py
@@ -5,4 +5,6 @@ d = { 'settings': { 'branch': 'master',
                 'doc_host': 'https://cega.github.io',
                 'git_url': 'https://github.com/cega/nbdev-hello-world',
                 'lib_path': 'nbdev_hello_world'},
-  'syms': {'nbdev_hello_world.core': {'nbdev_hello_world.core.foo': ('core.html#foo', 'nbdev_hello_world/core.py')}}}
+  'syms': { 'nbdev_hello_world.core': {'nbdev_hello_world.core.foo': ('core.html#foo', 'nbdev_hello_world/core.py')},
+            'nbdev_hello_world.modulo': { 'nbdev_hello_world.modulo.foo': ('core.html#foo', 'nbdev_hello_world/modulo.py'),
+                                          'nbdev_hello_world.modulo.say_hello': ('core.html#say_hello', 'nbdev_hello_world/modulo.py')}}}
